91ae3fb6d1e5ba400bae48202f69b4da70046d67
Per Møller <public.pfm@gmail.com>
2020-03-11 11:06:30 +0100

    Fix module resolution af comments

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@
 const path = require("path");
 
 module.exports = {
-  // Set debugging source maps to be "inline" for
-  // simplicity and ease of use
+  // Set debugging source maps to be seperat file
+  // An alternative is inline-source-map
   devtool: "source-map",
   // The application entry point
+  // Used for building the dependency graph that defines what gets included in the bundle
   entry: "./src/index.tsx",
   // Where to compile the bundle
   // By default the output directory is `dist`
