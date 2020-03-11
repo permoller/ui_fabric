1e1d4ada093fb8377c96e29135f66128fc274039
Per Møller <public.pfm@gmail.com>
2020-03-11 11:11:08 +0100

    UI Fabric added and some dependencies. moved to devDependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 import React from "react";
+import { PrimaryButton } from "office-ui-fabric-react";
 
-export const App = () => <div>React app with typescript build with webpack</div>;
+export const App = () => {
+    return (
+        <>
+            <div>React app with typescript build with webpack and included UI Fabric</div>
+            <PrimaryButton>UI Fabric Primary button</PrimaryButton>
+        </>)
+};
