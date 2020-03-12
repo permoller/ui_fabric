8de1f7f60e40ee251ad753bf1eaeedd5aef9345d
Per Møller <public.pfm@gmail.com>
2020-03-12 07:10:37 +0100

    TagPicker refresh demo simplified

--- src/TagPickerWithRefresh/TagPickerWithRefreshDemo.tsx
+++ src/TagPickerWithRefresh/TagPickerWithRefreshDemo.tsx
@@ -10,12 +10,6 @@ export const TagPickerWithRefreshDemo = () => {
     const picker2Ref = React.useRef<TagPickerWithResfreshBase>(null);
     const picker3Ref = React.useRef<TagPickerBase>(null);
 
-    const getRandomSuggestionsAsync = async () => {
-        console.log("fetching suggestions");
-        await timeoutAsync(2000);
-        return getRandomStrings().map(s => ({ key: s, name: s }));
-    }
-
     const refreshSuggestionsUsingCastToAnyToAccessProtectedMembers = <T, P extends IBasePickerProps<T>>(picker: BasePicker<T, P>) => {
         // to be able to access protected members without Typescript errors we cast to any
         // this metod can be used with all components that inherit from BasePicker
@@ -31,37 +25,34 @@ export const TagPickerWithRefreshDemo = () => {
             p.updateValue(value);
         }
     }
-
-    function refreshSuggestionsUsingBindingToThisToAccessProtectedMembers(this: TagPickerBase) {
+    const refreshSuggestionsUsingBindingToThisToAccessProtectedMembers = (picker: TagPickerBase) => {
         // this method does not work with the generic PickerBase
-        if (!this.state.suggestionsVisible) return;
-        const value = this.input.current.value;
-        if (value === '' && (this.props.onEmptyResolveSuggestions || this.props.onEmptyInputFocus)) {
-            // calls onEmptyResolveSuggestions or onEmptyInputFocus and updates the list of suggestions
-            this.onEmptyInputFocus();
-        } else {
-            // calls onResolveSuggestions and updates the list of suggestions
-            this.updateValue(value);
+        const refreshSuggestions = function (this: TagPickerBase) {
+            if (!this.state.suggestionsVisible) return;
+            const value = this.input.current.value;
+            if (value === '' && (this.props.onEmptyResolveSuggestions || this.props.onEmptyInputFocus)) {
+                // calls onEmptyResolveSuggestions or onEmptyInputFocus and updates the list of suggestions
+                this.onEmptyInputFocus();
+            } else {
+                // calls onResolveSuggestions and updates the list of suggestions
+                this.updateValue(value);
+            }
         }
+        refreshSuggestions.bind(picker)();
     }
 
-    const getRandomSuggestionsAndScheduleRefreshOfPicker1Async = async () => {
-        const tags = await getRandomSuggestionsAsync();
-        setTimeout(() => refreshSuggestionsUsingCastToAnyToAccessProtectedMembers(picker1Ref.current), 3000);
-        return tags;
+
+    const getRandomSuggestionsAsync = async () => {
+        console.log("fetching suggestions");
+        await timeoutAsync(2000);
+        return getRandomStrings().map(s => ({ key: s, name: s }));
     }
 
-    const getRandomSuggestionsAndScheduleRefreshOfPicker2Async = async () => {
-        const tags = await getRandomSuggestionsAsync();
-        setTimeout(() => picker2Ref.current.refreshSuggestionsUsingSubclassToAccessProtectedMembers(), 3000);
-        return tags;
-    }
-
-    const getRandomSuggestionsAndScheduleRefreshOfPicker3Async = async () => {
-        const tags = await getRandomSuggestionsAsync();
-        setTimeout(() => refreshSuggestionsUsingBindingToThisToAccessProtectedMembers.bind(picker3Ref.current)(), 3000);
-        return tags;
-    }
+    setInterval(() => {
+        refreshSuggestionsUsingCastToAnyToAccessProtectedMembers(picker1Ref.current);
+        picker2Ref.current.refreshSuggestionsUsingSubclassToAccessProtectedMembers();
+        refreshSuggestionsUsingBindingToThisToAccessProtectedMembers(picker3Ref.current);
+    }, 5000);
 
     return (
         <>
@@ -70,7 +61,7 @@ export const TagPickerWithRefreshDemo = () => {
                 Der genereres tilfældige valgmuligheder hver gang listen af valgmuligheder vises.
                 Hentning af valgmuligheder tager et sekund. I dette sekund vises en spinner.
                 Der logges en besked til konsollen hver gang valgmulighederne hentes.
-                Efter valgmulighederne er hentet vises de i tre sekunder hvorefter TagPicker komponenten bliver bedt om at genindlæse listen af valgmuligheder.
+                Hvert 5. sekund vil TagPicker komponenterne bliver bedt om at genindlæse listen af valgmuligheder.
             </p>
             <p>
                 Måden de forskellige komponenter tvinges til at genindlæse valgmulighederne er den samme. Det sker ved at tilgå nogle protectede properties på PickerBase.
@@ -79,19 +70,19 @@ export const TagPickerWithRefreshDemo = () => {
             <p>Dette er en TagPicker hvor vi laver et cast til any for at tilgå protected felter.</p>
             <TagPicker
                 componentRef={picker1Ref}
-                onEmptyResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker1Async}
-                onResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker1Async} />
+                onEmptyResolveSuggestions={getRandomSuggestionsAsync}
+                onResolveSuggestions={getRandomSuggestionsAsync} />
             <p>Dette er en custom TagPicker som arver fra den normale TagPicker for at kunne tilgå de protectede felter. Men den er nødt til at importere noget diekte fra en fil langt inde i maven på office-ui-fabric-react for at undgå at skulle style komponenten manuelt.</p>
             <TagPickerWithRefresh
                 componentRef={picker2Ref}
-                onEmptyResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker2Async}
-                onResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker2Async} />
+                onEmptyResolveSuggestions={getRandomSuggestionsAsync}
+                onResolveSuggestions={getRandomSuggestionsAsync} />
 
             <p>Dette er en TagPicker hvor vi laver custom bind af en funktions this-parameter for at tilgå de protectede felter.</p>
             <TagPicker
                 componentRef={picker3Ref}
-                onEmptyResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker3Async}
-                onResolveSuggestions={getRandomSuggestionsAndScheduleRefreshOfPicker3Async} />
+                onEmptyResolveSuggestions={getRandomSuggestionsAsync}
+                onResolveSuggestions={getRandomSuggestionsAsync} />
         </>
     );
 }
