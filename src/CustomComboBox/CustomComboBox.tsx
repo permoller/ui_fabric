e3c6c49e8ba3e47db75a59937a60a0409ee2f084
Per Fuglsang Møller <pfm@unik.dk>
2020-03-25 21:29:49 +0100

    CustomComboBox can be controlled and uncontrolled

--- src/CustomComboBox/CustomComboBox.tsx
+++ src/CustomComboBox/CustomComboBox.tsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
-import { stringify } from "../utils";
 import { Callout, Stack, CommandButton, DirectionalHint } from "office-ui-fabric-react";
-
 
 export interface ICustomComboBoxOption {
     key: string,
     text: string
 }
 export interface ICustomComboBoxProps<T extends ICustomComboBoxOption> {
+    uncontrolled?: boolean
     options: T[];
+    label?: string;
+    placeholder?: string;
+    /**
+     * If uncontrolled selectedOption is only used as the initial value and then ignored
+     * */
+    selectedOption?: T | undefined;
+    onChange?: (option: T | undefined) => void;
 }
 
 interface IBaseState {
     isOptionListOpen: boolean;
-    focusInput?: boolean;
+    selectInputText?: boolean;
 }
 
 interface ISearchState extends IBaseState {
@@ -28,7 +34,6 @@ interface IDisplayState<T extends ICustomComboBoxOption> extends IBaseState {
 
 type IState<T extends ICustomComboBoxOption> = ISearchState | IDisplayState<T>;
 
-const log = console.log;
 
 class Controller<T extends ICustomComboBoxOption> {
     public constructor(
@@ -36,24 +41,51 @@ class Controller<T extends ICustomComboBoxOption> {
         public state: IState<T>,
         public setState: React.Dispatch<React.SetStateAction<IState<T>>>,
         public inputRef: React.RefObject<HTMLInputElement>) {
-        
     }
 
-    public scheduleOpenOptionListOnFocus: boolean = true;
+    // normally when input gets focus we open the options list
+    private scheduleOpenOptionListOnInputFocus: boolean = true;
 
-    public sheduleFocusInput = () => {
-        this.setState(s => ({...s, focusInput:true}));
-    }
-
-    public componentDidUpdate = () => {
-        log("componentDidUpdate", this.state.focusInput, this.inputRef.current);
-        if(this.state.focusInput) {
-            this.setState(s => ({...s, focusInput:false}));
-            this.scheduleOpenOptionListOnFocus = false;
-            // det ser ud til at onInputFocus ikke bliver kaldt her når der vælges en option... måske fordi den allerede har fokus
-            this.inputRef.current && this.inputRef.current.focus();
-            this.scheduleOpenOptionListOnFocus = true;
+    public selectInputTextIfScheduled = () => {
+        if(this.state.selectInputText) {
+            // TODO: Can we avoid the extra render this triggers
+            this.setState(s => ({...s, selectInputText:false}));
+            this.focusInputAndSelectTextWithoutOpeningOptionList();
         }
+    }
+
+    public focusInputAndSelectTextWithoutOpeningOptionList = () => {
+        // normally when input gets focus it will open the options list
+        // we temprarely disable that function with a flag
+        this.scheduleOpenOptionListOnInputFocus = false;
+
+        this.focusInputAndSelectText();
+
+        // set the flag back to normal, so the option list will open the next time the input gets focus
+        this.scheduleOpenOptionListOnInputFocus = true;
+    }
+    
+    public doesInputHaveFocus = () => document.activeElement === this.inputRef.current;
+
+    public isControlled = () => !this.props.uncontrolled;
+
+    private focusInputAndSelectText = () => {
+        if(this.inputRef.current) {
+            // TODO: Not sure if calling focus is needed or if select always moves focus to the input
+            // and triggers focus event in all browsers
+            if(!this.doesInputHaveFocus()){
+                this.inputRef.current.focus();
+            }
+            this.inputRef.current.select();
+        }
+    }
+
+    public notifyOfChange = (option: T | undefined) => {
+        this.props.onChange && this.props.onChange(option);
+    }
+    
+    public scheduleSelectInputText = () => {
+        this.setState(s => ({...s, selectInputText:true}));
     }
 
     public scheduleDisplayState = (option: T | undefined) => {
@@ -72,83 +104,142 @@ class Controller<T extends ICustomComboBoxOption> {
         this.setState(s => ({...s, isOptionListOpen: false}));
     }
 
-    public getTextForInput = () => this.state.mode === "search" ? this.state.searchText : this.state.option?.text ?? "";
+    public getTextForInput = () =>
+        this.state.mode === "search"
+            ? this.state.searchText
+            : this.getSelectedOption()?.text ?? "";
 
     public getCurrentOptions = () => {
         const state = this.state;
         const options = this.props.options;
 
-        return state.mode !== "search"
-            ? options
-            : options.filter(option => option.text.toLocaleLowerCase().indexOf(state.searchText.toLocaleLowerCase()) >= 0);
+        // if we are searcing then filter the options else return alle the options
+        return state.mode === "search"
+            ? options.filter(option => option.text.toLocaleLowerCase().indexOf(state.searchText.toLocaleLowerCase()) >= 0)
+            : options;
     }
-    
-    public onInputFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-        log("INPUT - onFocus", this.scheduleOpenOptionListOnFocus);
-        if(this.scheduleOpenOptionListOnFocus) {
+
+    public getSelectedOption = () => {
+        // if we are not in display mode there is no selected option
+        if(this.state.mode !== "display") {
+            return undefined;
+        }
+        // if controlled the option given through props is the current one.
+        // if uncontrolled the option in the state is the current one
+        return this.isControlled() ? this.props.selectedOption : this.state.option;
+    }
+
+    public onInputFocus = () => {
+        this.focusInputAndSelectText();
+        if(this.scheduleOpenOptionListOnInputFocus) {
             this.scheduleOpenOptionList();
         }
-        this.inputRef.current && this.inputRef.current.select();
     }
 
     public onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        log("Input - onChange", e.target.value);
+        console.log("on input change");
         this.scheduleSearchState(e.target.value);
+        // if uncontrolled the state-change will trigger notification on next render
+        // if controlled we need to notify that no option is selected
+        if(this.isControlled()) {
+            this.notifyOfChange(undefined);
+        }
     }
 
     public onOptionListDismiss = () =>  {
-        // log("OptionList - onDismiss");
-        if(this.state.mode !== "display"){
+        if(this.state.mode !== "display") {
             this.scheduleDisplayState(undefined);
         }
         this.scheduleCloseOptionList();
     }
 
     public onOptionClick = (option: T) => {
-        log("Option - onClick", stringify(option));
-        this.scheduleDisplayState(option);
-        this.sheduleFocusInput();
+        if(this.isControlled()) {
+            // we switch back to displaying the option we currently got
+            this.scheduleDisplayState(this.props.selectedOption);
+            this.scheduleSelectInputText();
+            
+            // notify if the option has changed
+            if(this.props.selectedOption !== option){
+                this.notifyOfChange(option);
+            }
+            
+            // we do not update the state until we actually get the new selected option through the props
+            // but we do want to make sure the input element has focus
+            // so that the text will be selected when/if we get a new selected option
+            this.focusInputAndSelectTextWithoutOpeningOptionList();
+
+        } else {
+            // If uncontrolled we need to update the state to display the selcted option
+            // and delay notification until the new state is rendered
+            this.scheduleDisplayState(option);
+            this.scheduleSelectInputText();
+        }
     };
 }
 
 export const CustomComboBox = function<T extends ICustomComboBoxOption>(props: ICustomComboBoxProps<T>) {
     const [state, setState] = useState<IState<T>>(() => ({
         mode:"display",
-        option: undefined,
-        isOptionListOpen: false
+        option: props.selectedOption,
+        isOptionListOpen: false,
     }));
-    
-    log("CustomComboBox - render", stringify(state));
-
+   
     const controller = new Controller<T>(props, state, setState, useRef<HTMLInputElement>(null));
-    useEffect(() => {
-        controller.componentDidUpdate();
-    });
 
+    // get the option that should be selected
+    const selectedOption = controller.getSelectedOption();
+    
+    // if we are uncontrolled we need to notify when the selected option has changed after it is rendered
+    useEffect(() => {
+        if(!controller.isControlled()) {
+            controller.notifyOfChange(selectedOption);
+        }
+    }, [selectedOption])
+    
+    // if we are controlled and get a new selected option from props
+    // we need to update the local state, but not if the user is curently searching for another option
+    if(controller.isControlled() && state.mode === "display" && state.option != selectedOption) {
+        controller.scheduleDisplayState(selectedOption);
+        // we only want to select the text if the input already has focus
+        // else focus might be stolen from somewhere else and we do not want that
+        if(controller.doesInputHaveFocus()) {
+            controller.scheduleSelectInputText();
+        }
+    }
+    
+    useEffect(controller.selectInputTextIfScheduled);
+    
     return (
         <div>
-            <Input {...controller} />
-            <OptionsList {...controller} />
+            <Label controller={controller} />
+            <Input controller={controller} />
+            <OptionsList controller={controller} />
         </div>
     );
 }
 
-const Input = function<T extends ICustomComboBoxOption>(controller: Controller<T>) {
+const Label = function<T extends ICustomComboBoxOption>({controller}: {controller: Controller<T>}) {
+    // TODO: set target for label to input
+    return controller.props.label ? <label>{controller.props.label}</label> : null;
+}
+
+const Input = function<T extends ICustomComboBoxOption>({controller}: {controller: Controller<T>}) {
     return <input
+        type="text"
         ref={controller.inputRef}
         value={controller.getTextForInput()}
         onFocus={controller.onInputFocus}
-        onChange={controller.onInputChange}
-        onBlur={() => log("Input - onBlur")}
+        onInput={controller.onInputChange}
+        placeholder={controller.props.placeholder}
         />
 }
 
-const OptionsList = function<T extends ICustomComboBoxOption>(controller: Controller<T>) {
-    log("OptionList - render", controller.state.isOptionListOpen)
+const OptionsList = function<T extends ICustomComboBoxOption>({controller}: {controller: Controller<T>}) {
     if(!controller.state.isOptionListOpen) {
         return null;
     }
-
+    
     return (
         <Callout
             // open list at the input-field
@@ -170,8 +261,6 @@ const OptionsList = function<T extends ICustomComboBoxOption>(controller: Contro
     );
 }
 
-const Option = function<T extends ICustomComboBoxOption>(props: {controller: Controller<T>, option: T}) {
-    const {controller, option} = props;
-    
+const Option = function<T extends ICustomComboBoxOption>({controller, option}: {controller: Controller<T>, option: T}) {
     return (<CommandButton onClick={() => controller.onOptionClick(option)}>{option.text}</CommandButton>);
 }
--- src/CustomComboBox/CustomComboBoxDemo.tsx
+++ src/CustomComboBox/CustomComboBoxDemo.tsx
@@ -1,13 +1,165 @@
-import React from "react";
+import React, { useState } from "react";
 import { WithLog } from "../WithLog/WithLog";
 import { CustomComboBox } from "./CustomComboBox";
 
 const options = ["red", "green", "blue"].map(s => ({key:s, text:s}));
 
 export const CustomComboBoxDemo = () => {
-return <WithLog>{(log) => {
+    const [selectedOption, setSelectedOption] = useState<typeof options[0] | undefined>(undefined);
     return (<>
-    <p>Custom ComboBox med søgning</p>
-    <CustomComboBox options={options} /></>);
-}}</WithLog>;
+        <p>Custom ComboBox with search</p>
+
+        <CustomComboBox 
+            label="uncontrolled"
+            uncontrolled
+            options={options}
+            placeholder="select a color"
+            onChange={(o) => console.log("uncontrolled changed", o)} />
+        <br/>
+
+        <CustomComboBox
+            label="controlled"
+            options={options}
+            placeholder="select a color"
+            selectedOption={selectedOption}
+            onChange={(o) => {
+                console.log("controlled changed", o);
+                setSelectedOption(o);
+            }} />
+        <br/>
+        <button onClick={() => setTimeout(() => setSelectedOption(options[1]), 2000)}>Set controlled to green after 2 second delay</button>
+
+        <h2>component elements and states</h2>
+        <h3>component overall</h3>
+        <ul>
+            <li>controlled or uncontrolled</li>
+            <li>hasFocus or not</li>
+            <li>option is selected or not</li>
+        </ul>
+        <h3>input field</h3>
+        <ul>
+            <li>empty or has text or shows placholder (empty if not placeholder is provided)</li>
+            <li>caret or not (the vertical line that indicates where the letter you type will be inserted)</li>
+            <li>when has text
+                <ul>
+                    <li>text is selected or not</li>
+                </ul>
+            </li>
+            <li></li>
+        </ul>
+        <h3>options list</h3>
+        <ul>
+            <li>closed</li>
+            <li>open with all options</li>
+            <li>open with filtered options</li>
+        </ul>
+        <h3>option</h3>
+        <ul>
+            <li>highlighted or not</li>
+        </ul>
+        <h3>button</h3>
+        TODO
+        <h2>behaviour</h2>
+        <h3>when the component does not have focus</h3>
+        <ul>
+            <li>if no option is selected
+                <ul>
+                    <li>options list - closed</li>
+                    <li>input field - no caret</li>
+                    <li>input field - show placeholder text</li>
+                    <li>input field - text is not selected</li>
+                </ul>
+            </li>
+            <li>if option is selected
+                <ul>
+                    <li>options list - closed</li>
+                    <li>input field - no caret</li>
+                    <li>input field - show text from selected option</li>
+                    <li>input field - text is not selected</li>
+                </ul>
+            </li>
+        </ul>
+        <h3>when the component gets focus (both by tabing into field and clicking unfocused field)</h3>
+        <ul>
+            <li>if no option is selected
+                <ul>
+                    <li>options list - open with all options</li>
+                    <li>input field - show caret</li>
+                    <li>input field - show placeholder text</li>
+                </ul>
+            </li>
+            <li>if option is selected
+                <ul>
+                    <li>options list - open with all options</li>
+                    <li>input field - show caret</li>
+                    <li>input field - show text from selected option</li>
+                    <li>input field - text is selected <span style={{color:"red"}}>does not work when clicking</span></li>
+                </ul>
+            </li>
+        </ul>
+        <h3>when the component has focus</h3>
+        <ul>
+            <li>when user enters (or deletes or pastes) text in input field <span style={{color:"red"}}>the browser clear-button in Edge does not trigger the expected events</span>
+                <ul>
+                    <li>no option is selected (notify if changed)</li>
+                    <li>options list - open with filtered options</li>
+                    <li>input field - show caret</li>
+                    <li>input field - shows entered text (or placeholder if text was deleted)</li>
+                    <li>input field - text is not selected</li>
+                </ul>
+            </li>
+            <li>when user clicks with mouse in input field
+                <ul>
+                    <li>options list - remains unchanged <span style={{color:"red"}}>should the list open if it was closed?</span></li>
+                    <li>input field - caret is posistioned</li>
+                    <li>input field - shows entered text</li>
+                    <li>input field - part of text is selected if dragging</li>
+                </ul>
+            </li>
+            <li>when mouse-cursor hovers over option
+                <ul>
+                    <li>options list - remains unchanged</li>
+                    <li>input field - remains unchanged</li>
+                    <li>the option is highlighted</li>
+                </ul>
+            </li>
+            <li>when user clicks on an option
+                <ul>
+                    <li>clicked option is selected (notify if changed)</li>
+                    <li>options list - closed</li>
+                    <li>input field - show text from option</li>
+                    <li>input field - text is selected</li>
+                </ul>
+            </li>
+            <li>when arrow-down-key is pressed
+                <ul>
+                    <li>TODO</li>
+                </ul>
+            </li>
+            <li>when arrow-up-key is pressed
+                <ul>
+                    <li>TODO</li>
+                </ul>
+            </li>
+            <li>when enter-key is pressed
+                <ul>
+                    <li>TODO</li>
+                </ul>
+            </li>
+            <li>when tab-key is pressed
+                <ul>
+                    <li>TODO</li>
+                </ul>
+            </li>
+        </ul>
+        <h3>when the component loses focus (both by tabbing and clicking somewhere else)</h3>
+        <ul>
+            <li>same as when the component does not have focus</li>
+        </ul>
+        <h3>other</h3>
+        <ul>
+            <li>When controlled changing selectedOption property does not trigger onChange. It is assumed the controlling component knows about the change.</li>
+            <li><span style={{color:"red"}}>when first rendered the uncontrolled component calls onChange... it shuld not do that.</span></li>
+        </ul>
+    </>);
 }
